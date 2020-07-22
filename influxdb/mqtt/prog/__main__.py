e00c11d3eb2d8136125ac32486b5e286e9b0eeb7
KarpovaElena <52173441+KarpovaElena@users.noreply.github.com>
2020-07-22 19:33:01 +0300

    Update __main__.py

--- influxdb/mqtt/prog/__main__.py
+++ influxdb/mqtt/prog/__main__.py
@@ -13,6 +13,5 @@ app.router.add_get('/stop', ep.stop_server)
 app.router.add_get('/add', ep.add_var)
 app.router.add_get('/list_var', ep.list_var)
 app.router.add_get('/mq', ep.add_mosquitto)
-app.router.add_post('/auth', ep.auth)
 
 web.run_app(app, host='0.0.0.0', port=8010)
